62ca6e4ee17aa70dd16ee255e4b4d065adeabcac
vinhluong274 <vinhluong274@gmail.com>
2018-03-11 20:09:24 -0400

    Completed Midterm Project

--- SI364midterm.py
+++ SI364midterm.py
@@ -1,7 +1,3 @@
-###############################
-####### SETUP (OVERALL) #######
-###############################
-
 ## Import statements
 import os, requests, json
 from flask import Flask, render_template, session, redirect, url_for, flash, request
@@ -11,13 +7,10 @@ from wtforms.validators import Required, Length, Email, Regexp # Here, too
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager, Shell
 
-
-
 ## App setup code
 app = Flask(__name__)
 app.debug = True
 app.use_reloader = True
-
 ## All app.config values
 app.config['SECRET_KEY'] = "hardtoguessstring"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://vinhluong@localhost/364midterm"
@@ -42,15 +35,18 @@ def page_not_found(e):
 ##################
 ##### MODELS #####
 ##################
-
+#Four Models Total
+#This model stores any name that is entered into the Name Search Form.
+#This model will allow unlimited duplicates.
 class Name(db.Model):
     __tablename__ = "names"
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64))
 
     def __repr__(self):
-        return "{} -- (Search #: {})".format(self.name, self.id)
+        return "{} -- (Search # {})".format(self.name, self.id)
 
+#This model stores users' name and desired username.
 class User(db.Model):
     __tablename__ = "Users"
     user_id = db.Column(db.Integer, primary_key=True)
@@ -60,14 +56,7 @@ class User(db.Model):
     def __repr__(self):
         return "{} (UserID {})".format(self.username, self.user_id)
 
-class Places(db.Model):
-    __tablename__ = "Places"
-    zipcode = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(124))
-
-    def __repr__(self):
-        return "{} (Zipcode: {})".format(self.city, self.zipcode)
-
+#This database will store facts about certain places. It has a many to one relationship with the places model below.
 class Facts(db.Model):
     __tablename__ = "Facts"
     fact_id = db.Column(db.Integer, primary_key=True)
@@ -78,6 +67,15 @@ class Facts(db.Model):
     def __repr__(self):
         return "{} (Zipcode: {})".format(self.fact, self.place_id)
 
+#This stores information about zipcodes and names of the cities those codes belong to. This has a one to many relationship with the Facts model.
+#One place can have many interesting facts.
+class Places(db.Model):
+    __tablename__ = "Places"
+    zipcode = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(124))
+
+    def __repr__(self):
+        return "{} (Zipcode: {})".format(self.city, self.zipcode)
 
 ###################
 ###### FORMS ######
@@ -116,22 +114,11 @@ class ZipForm(FlaskForm):
 ###### VIEW FXNS ######
 #######################
 
-# @app.route('/', methods=['GET', 'POST'])
-# def home():
-#     form = NameForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with GET
-#     if form.validate_on_submit():
-#         name = form.name.data
-#         user = Name.query.filter_by(name=name).first()
-#         if user:
-#             return redirect(url_for('city_facts_form'))
-#         else:
-#             newname = Name(name=name)
-#             db.session.add(newname)
-#             db.session.commit()
-#         return redirect(url_for('user_info'))
-#     return render_template('base.html',form=form)
-
-#Main function
+#Main function. This is the home page and presents users with a form that asks for their name, username, zipcode, and a fun fact for the zipcode area they entered.
+#Depending on whether the user, username, or zipcode already exists in the databases a message will be displayed on the same page explaining such. New names, usernames, and zipcodes will be added to the database.
+#If there is a new zipcode, the function will make a request to Google's Goecoding API to gather city, state, and country details about the zipcode entered.
+#If name, username, zipcode, and fact entered all exist in the databases and are connected to the same user, the function will not store the data, but display that it was already entered.
+#No Identical Submission allowed.
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = UserForm()
@@ -140,39 +127,43 @@ def index():
         username = form.username.data
         zipcode = form.zipcode.data
         fact = form.fact.data
-        if User.query.filter_by(name=name, username=username).first():
-            user = User.query.filter_by(name=name, username=username).first()
-            print("User already exists!")
+        if (User.query.filter_by(name=name, username=username).first()) and (Facts.query.filter_by(fact=fact, place_id=zipcode).first()):
+            flash("An identical submission has already been made!")
+            return render_template('home.html', form=form)
         else:
-            user = User(name=name, username=username)
-            db.session.add(user)
-            db.session.commit()
-            flash("New user info added!")
-        if (Places.query.filter_by(zipcode=zipcode).first()):
-            flash("Zipcode of place already stored!")
-            place = Places.query.filter_by(zipcode=zipcode).first()
-            newfact = Facts(fact=fact, place_id=place.zipcode, poster_id=user.user_id)
-            db.session.add(newfact)
-            db.session.commit()
-            flash("Added new fact!")
-        else:
-            url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode
-            data = requests.get(url)
-            city = data.json()["results"][0]["formatted_address"]
-            place = Places(zipcode=zipcode, city=city)
-            db.session.add(place)
-            db.session.commit()
-            flash("New place added!")
-            newfact = Facts(fact=fact, place_id=place.zipcode, poster_id=user.user_id)
-            db.session.add(newfact)
-            db.session.commit()
-            flash("Added new fact!")
-            return redirect(url_for("index"))
-
-    errors = [v for v in form.errors.values()]
+            if User.query.filter_by(name=name, username=username).first():
+                user = User.query.filter_by(name=name, username=username).first()
+                flash("User already exists")
+            else:
+                user = User(name=name, username=username)
+                db.session.add(user)
+                db.session.commit()
+                flash("New user info added!")
+            if (Places.query.filter_by(zipcode=zipcode).first()):
+                flash("Zipcode of place already stored!")
+                place = Places.query.filter_by(zipcode=zipcode).first()
+                newfact = Facts(fact=fact, place_id=place.zipcode, poster_id=user.user_id)
+                db.session.add(newfact)
+                db.session.commit()
+                flash("Added new fact!")
+            else:
+                url ="https://maps.googleapis.com/maps/api/geocode/json?address="+ zipcode + "&key=AIzaSyDn5BePWvs0CpWjZoSKyYaHSZYQSqCHpAc"
+                data = requests.get(url)
+                city = data.json()["results"][0]["formatted_address"]
+                place = Places(zipcode=zipcode, city=city)
+                db.session.add(place)
+                db.session.commit()
+                flash("New zipcode added!")
+                newfact = Facts(fact=fact, place_id=place.zipcode, poster_id=user.user_id)
+                db.session.add(newfact)
+                db.session.commit()
+                flash("Added new fact!")
+                return render_template('home.html', form=form)
+    errors = form.errors.values()
     if len(errors) > 0:
-        flash("ERROR(S) IN FORM SUBMISSION! - " + str(errors))
-    return render_template('base.html', form=form)
+        for i in errors:
+            flash("Error in Submission: " + str(i))
+    return render_template('home.html', form=form)
 
 #View All Facts that have been submitted.
 @app.route('/allfacts')
@@ -183,6 +174,7 @@ def all_facts():
         place = Places.query.filter_by(zipcode=i.place_id).first()
         facts.append((i.fact,place.city))
     length = len(facts)
+    print(facts)
     return render_template('all-facts.html', facts=facts, length=length)
 
 #Additional WTForm to that uses POST to search facts by zipcode and displays results them on the same page.
@@ -202,9 +194,10 @@ def zip_search():
             flash("No facts matching {}".format(zipcode))
             return redirect(url_for("zip_search"))
 
-    errors = [v for v in form.errors.values()]
+    errors = form.errors.values()
     if len(errors) > 0:
-        flash("ERROR(S) IN FORM SUBMISSION! - " + str(errors))
+        for i in errors:
+            flash("Error in Submission: " + str(i))
     return render_template('zip-search.html', form=form)
 
 
@@ -215,6 +208,7 @@ def name_search():
     form = NameForm()
     return render_template("name-search.html", form=form)
 
+#This is the redirect page for the previous function. It will use data from the previous form to query the database and display any facts entered by the name searched.
 @app.route('/name-results', methods=['POST', 'GET'])
 def name_results():
     name = request.args["name"]
@@ -228,19 +222,17 @@ def name_results():
         place = Places.query.filter_by(zipcode=poster.place_id).first()
         facts.append((poster.fact, place.city))
     length=len(facts)
-
     return render_template("name-results.html", name=name, length=length, facts=facts)
 
-#Route to view names of all who have filled out the home form
+#Route to view all of the names that have been entered into the name search form. Duplicates are allowed. This is a slight adaptation of the code that was given to us.
 @app.route('/names')
 def all_names():
     names = Name.query.all()
     return render_template('name_example.html',names=names)
 
 ## Code to run the application...
-if __name__ == "__main__":
-    db.create_all()
-    app.run(use_reloader=True,debug=True)
-
 # Put the code to do so here!
 # NOTE: Make sure you include the code you need to initialize the database structure when you run the application!
+if __name__ == "__main__":
+    db.create_all()#creates all the databases needed if not any
+    app.run(use_reloader=True,debug=True)
